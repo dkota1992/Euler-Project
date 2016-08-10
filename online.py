722f37c57625029429c6c692ebc96521f4c3c5e4
dkota1992 <dkota1992@gmail.com>
2016-08-10 17:40:25 -0500

    Add files via upload

---
+++ online.py
@@ -0,0 +1,27 @@
+from time import time
+import math
+start = time()
+
+
+num = 10000000
+
+def prime_primesquares(num):
+    A = [False]*2+ [True]*num
+    k = math.sqrt(num)
+    for i,j in enumerate(A):
+        if j:
+            yield i
+            for n in range(i*i, num, i): 
+                A[n] = False
+                
+            if i<k: A[i**2] = True
+result = 1                   
+j = 1
+k = 1
+g = prime_primesquares(num)
+while j<500500+ 1:    
+    k = g.next()
+    j = j+1
+    result = result*k%500500507
+
+print(j,result,time()-start)
